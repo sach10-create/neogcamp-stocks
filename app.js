a9f34011579bcdad8517bdd561c7726b6d8ad6ff
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-26 21:33:18 +0530

    updated app.js with logic

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ const initialPrice = document.querySelector(".initial-price");
 const quantity = document.querySelector(".quantity");
 const currentPrice = document.querySelector(".current-price");
 const button = document.querySelector(".submit-btn");
+const output = document.querySelector(".output-box")
 
-function clickHandler(){
-    var ini = initialPrice.value;
-    var quan = quantity.value;
-    var curr = currentPrice.value;
+function clickHandler() {
+    const ini = initialPrice.value;
+    const quan = quantity.value;
+    const curr = currentPrice.value;
     calculateStocksResult(ini, quan, curr);
 }
 
-function calculateStocksResult(inital , quantity , current){
-    console.log("hi")
+function calculateStocksResult(initial, quantity, current) {
+    if ((initial.length == 0) && (quantity.length == 0) && (current.length == 0)) {
+        alert('Enter all the values');
+    } else {
+        if (initial > current) {
+            var loss = (inital - current) * quantity;
+            var lossPercent = (loss / inital) * 100;
+            console.log("loss")
+        } else if (current > initial) {
+            var profit = (current - initial) * quantity;
+            var profitPercent = (profit / initial) * 100;
+            console.log("profit")
+        } else {
+
+        }
+    }
 }
 
-button.addEventListener("click",clickHandler);
+button.addEventListener("click", clickHandler);
