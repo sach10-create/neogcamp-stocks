1429b6d5e085b78bf299e9715d307bb6cb674836
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-26 21:11:01 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const initialPrice = document.querySelector(".initial-price");
+const quantity = document.querySelector(".quantity");
+const currentPrice = document.querySelector(".current-price");
+const button = document.querySelector(".submit-btn");
+
+function clickHandler(){
+    console.log("hi");
+}
+
+button.addEventListener("click",clickHandler);
