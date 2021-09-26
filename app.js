924756b1a67d1c010b18ddd23ed2d88575d443f8
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-26 21:13:55 +0530

    updated app.js

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const currentPrice = document.querySelector(".current-price");
 const button = document.querySelector(".submit-btn");
 
 function clickHandler(){
-    console.log("hi");
+    var ini = initialPrice.value;
+    var quan = quantity.value;
+    var curr = currentPrice.value;
+    calculateStocksResult(ini, quan, curr);
+}
+
+function calculateStocksResult(inital , quantity , current){
+    console.log("hi")
 }
 
 button.addEventListener("click",clickHandler);
